3c2d4d592e4c0239e8415cff245002273bd77920
Gon <gon.mendez13@gmail.com>
2023-03-08 15:35:43 -0300

    removed logs

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,6 @@
 import { transformAboutMeData } from "../utils";
 
 export async function getAboutMeItems() {
-  console.log(
-    process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-    process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
-  );
-
   const res = await fetch(
     `https://preview.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/master/entries?access_token=${process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN}`
   );
